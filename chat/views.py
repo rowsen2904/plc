ba5fd74b215fc0030b2ab548b9685909e5b12366
Rowsen <bayramovw0@gmail.com>
2023-03-13 12:23:10 +0500

    some changes

--- chat/views.py
+++ chat/views.py
@@ -29,7 +29,7 @@ def index(request):
         message.save()
         return redirect('chat:index')
     
-    return HttpResponse('Not valid')
+    return redirect("chat:index")
 
 @allowed_users(allowed_roles=['superadmin'])
 def cleanChat(request):
